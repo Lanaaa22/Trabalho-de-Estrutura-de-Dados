3629fdac0c1ac5e2ee3e42672fffed334ba333ba
Ilanna Cardoso <91700396+Lanaaa22@users.noreply.github.com>
2025-10-27 11:12:30 -0300

    Add files via upload
    Começar corrigindo o erro no vetor em tad_bdpartidas.c

---
+++ trab-Estdade/TADs/tad_bdtimes.c
@@ -0,0 +1,8 @@
+#include "tad_bdtimes.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct bdtime {
+    int id;
+};
---
+++ trab-Estdade/TADs/tad_time.c
@@ -0,0 +1,9 @@
+#include "tad_time.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct time {
+    int id; //id dos times
+    char nome[50]; //nome do time
+};
