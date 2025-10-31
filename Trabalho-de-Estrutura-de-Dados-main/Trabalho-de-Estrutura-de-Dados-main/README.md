77e7bea97598417b4b8fd9aff043518e7cda0c2e
Ilanna Cardoso <91700396+Lanaaa22@users.noreply.github.com>
2025-10-31 11:25:52 -0300

    Delete Trabalho-de-Estrutura-de-Dados-main/Trabalho-de-Estrutura-de-Dados-main directory

--- Trabalho-de-Estrutura-de-Dados-main/Trabalho-de-Estrutura-de-Dados-main/README.md
+++
@@ -1,2 +0,0 @@
-# Trabalho-de-Estrutura-de-Dados
-Trabalho feito para a matéria de estrutura de dados em dupla
--- Trabalho-de-Estrutura-de-Dados-main/Trabalho-de-Estrutura-de-Dados-main/trab-Estdade/TADs/system.h
+++
@@ -1,8 +0,0 @@
-#ifndef SYSTEM_H
-#define SYSTEM_H
-
-void lobby(); // Função para escolha das funcionalidades
-
-void LimpaTela(); //Função para limpar a tela do terminal
-
-#endif
